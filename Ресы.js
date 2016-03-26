70c5cec477a3619b651387808f6cdb6fd79ee44f
punkromain <beraidag@gmail.com>
2016-03-26 03:17:14 +0000

    Update Ресы.js

--- Ресы.js
+++ Ресы.js
@@ -1,17 +1,20 @@
 // ==UserScript==
 // @name         dream_Bot
-// @description  хзхзmod1
-// @author       soloromail@gmail.com, Deimon, ник "гyрy"
+// @description  полныйmod1
+// @author       soloromail@gmail.com, Deimon, Ненчык-патыр, Старовер
 // @match        http://dreamwar.ru/*
 // ==/UserScript==
 /* jshint -W097 */
+if(location.href.match('dreamwar.ru')) {
+speed=321;
+vst = 1;//1 - вступаем в заявки, 0 - нет
 //###########
-//##хз##
+//##ФУНКЦИИ##
 //###########
-//в рот мне ноги
+//ТЕКСТ
 function legendsFindText(tx) {if(document.body.innerHTML.match(tx)) {return true;} else {return false;}}
-//и говорить
-function legendsFindLink(tx) {l=0;for(i=0;i<document.links.length;i++){if(document.links[i].text.match(tx)&&!document.links[i].text.match('������� ���')) {l=document.links[i].href; break;}} if(l!=0) {return l;} else {return false;}}
+//ССЫЛКИ
+function legendsFindLink(tx) {l=0;for(i=0;i<document.links.length;i++){if(document.links[i].text.match(tx)&&!document.links[i].text.match('Собрать все')) {l=document.links[i].href; break;}} if(l!=0) {return l;} else {return false;}}
 function legendsFindLinkA(tx) {l=0;for(i=0;i<document.links.length;i++){if(document.links[i].text==tx) {l=document.links[i].href; break;}} if(l!=0) {return l;} else {return false;}}
 function legendsFindLinkAn(tx) {for(i=0;i<document.links.length;i++){if(document.links[i].text==tx) {l=i; break;}} return l;}
 function legendsFindLinkNoEnd(tx) {l=0;for(i=0;i<document.links.length-1;i++){if(document.links[i].text.match(tx)) {l=document.links[i].href; break;}} if(l!=0) {return l;} else return false;}
@@ -19,9 +22,9 @@ function legendsFindLinkEnd(tx) {l=0;for(i=0;i<document.links.length-1;i++){if(d
 function legendsFindLinkD(tx,bx) {l=0;for(i=0;i<document.links.length;i++){if(document.links[i].innerHTML.match(bx)&&document.links[i].text.match(tx)) {l=document.links[i].href; break;}} if(l!=0) {return l;} else {return false;}}
 //function legendsFindLinkD(tx,bx) {l=0;for(i=0;i<document.links.length;i++){if(document.links[i].innerHTML.match(bx)&&document.links[i].text.match(tx)) {l=document.links[i].href; break;}} if(l!=0) {return l;} else {return false;}}
 function legendsFindLinker(tx) {l=0;for(i=0;i<document.links.length;i++){if(document.links[i].href.match(tx)) {l=document.links[i].href; break;}} if(l!=0) {return l;} else {return false;}}
-/*про этy херню даже слyшать не хочy*/
+/*Переход по ссылке с интервалом*/
 function legendsLink(tx, sec) {if(tx!="") {setTimeout(function(){location.href=tx;}, sec);}}
-/*и это херня*/
+/*Проверка значения хранилища*/
 function legendsIsStorage(name, tx) {if(localStorage.getItem(name)==tx) { return true; } else { return false;}}
 function legendsGet(name) {return localStorage.getItem(name);}
 
@@ -29,7 +32,7 @@ function legendsGet(name) {return localStorage.getItem(name);}
 function legendsSetInput(n,a) {if(n!=""&&a!="") {document.getElementsByTagName('input')[n].value=a;}}
 
 function legendsCoord(hh) {for(var i=0;i<document.getElementsByTagName('b').length;i++){if(document.getElementsByTagName('b')[i].innerHTML.match("/")){var sdf=document.getElementsByTagName('b')[i].innerHTML;var hgf=sdf.split('/');y=parseInt(hgf[0]);x=parseInt(hgf[1]);i=document.getElementsByTagName('b').length;}}if(hh=="x") {return x;} if(hh=="y") {return y;}}
-/*тоже херня*/
+/*Назначение для хранилища*/
 function legendsSet(name, tx) {if(localStorage.setItem(name, tx)) { return true; } else { return false;}}
 function legendsI(name, tx) {document.getElementById(name).innerHTML=tx;}
 function legendsClick(tx, sec) {for(var q=0;q<document.forms.length;q++){for(var y=0;y<document.forms[q].elements.length;y++){if (document.forms[q].elements[y].value.match(tx)){var go = document.forms[q].elements[y];setTimeout(function(){go.click();}, sec);}}}}
@@ -56,12 +59,19 @@ function legendsSelect(n,a) {objSel = document.getElementsByName(n)[0];objSel.op
 function legendsSelCount(n) {objSel = document.getElementsByName(n)[0]; return objSel.options.length;}
 
 //###########
-//##херная какая-то##
+//##ФУНКЦИИ##
 //###########
 
 if(legendsFindText('Service Temporarily Unavailable')||legendsFindText('Bad Gateway')||legendsFindText('internal server error')){
+//Заметил, что частенько на баффах выпадает 503(502,500) ошибка. Обновляем страницу
 legendsLink(window.location.href,1);
 }
-if(!legendsFindText('Добыча ресурсов')) legendsLink(legendsFindLink("[Начать добычу]"),50000)){
+
+if(!legendsFindText('Добыча ресурсов')) legendsLink(legendsFindLink("[Начать добычу]"),50000)){ 
 legendsLink(legendsFindLink("[Обновить]"),10000); 
 }
+
+
+
+}
+// Your code here...
